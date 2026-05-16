b63e483dd80ca489152e92d65fbffeb2205f78ba
Weston Hinton <wbhinton@gmail.com>
2026-05-16 18:13:10 -0400

    chore: lower minimum SDK version to 21 in build configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "io.datarx.elrsmobile"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 28
+        minSdk = 21
         targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
